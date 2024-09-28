bb4b080f3308f122aa4c4aedba51c07a7ba8a72f
mauricio-chirino <mchirino@aniracloud.com>
2024-09-28 14:32:47 -0400

    agregadas las gemas faker y annotate

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id          :bigint           not null, primary key
+#  usename     :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Tweet < ApplicationRecord
 end
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id          :bigint           not null, primary key
+#  usename     :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class TweetTest < ActiveSupport::TestCase
