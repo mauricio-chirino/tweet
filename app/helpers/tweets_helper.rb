ef2ec23eaae08823e89a8ed48f2393cb72610922
mauricio-chirino <mchirino@aniracloud.com>
2024-09-29 19:41:40 -0400

    creados los iconos y formato tweeter

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,13 @@
 module TweetsHelper
+require 'json'
+
+ def extract_username()
+
+ end
+
+  def formatted_created_at(tweet)
+    created_at = Time.parse(tweet)
+    created_at.strftime("%A, %d de %B")
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+
+
+50.times do
+  Tweet.create(
+    usename: Faker::Twitter.user,
+    description: Faker::Lorem.sentence(word_count: 10)
+  )
+end
